70b2bad28a3bdeb8dc4edda0376d2ed483a477aa
Ivan <ivdjuran@gmail.com>
2022-03-30 16:12:25 +0200

    Dodat je izbor desktop okruzenja

--- nastavak.sh
+++ nastavak.sh
@@ -20,6 +20,7 @@ else
     grub-install /dev/sda
     grub-mkconfig -o /boot/grub/grub.cfg
 fi
+
 fallocate -l 3G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
@@ -29,7 +30,57 @@ useradd -m -g users -G wheel -s /bin/bash ivan
 passwd ivan
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server --noconfirm
-pacman -S --needed  sddm --noconfirm
-pacman -S plasma dolphin konsole --noconfirm
-sudo systemctl enable sddm
-echo "Current=breeze" >> /usr/lib/sddm/sddm.conf.d/default.conf
+echo "1 - GNOME"
+echo "2 - KDE plasma"
+echo "3 - XFCE"
+echo "4 - Cinamon"
+echo "5 - Mate"
+echo "6 - i3"
+echo "7 - Awesome"
+echo "8 - Deepin"
+echo "9 - LXDE"
+read IZBOR
+
+case $IZBOR in
+    1)
+    echo "exec gnome-session" > ~/.xinitrc
+     pacman -S gnome
+    ;;
+    2)
+    pacman -S --needed  sddm --noconfirm
+    pacman -S plasma dolphin konsole --noconfirm
+    echo "Current=breeze" >> /usr/lib/sddm/sddm.conf.d/default.conf
+     systemctl enable sddm
+    ;;
+    3)
+    pacman -S xfce4 lightdm lightdm-gtk-greeter
+    echo "exec startxfce4" > ~/.xinitrc
+    systemctl enable lightdm
+    ;;
+    4)
+    echo "exec cinnamon-session" > ~/.xinitrc
+     pacman -S cinnamon mdm
+    systemctl enable mdm
+    ;;
+    5)
+    echo "exec mate-session" > ~/.xinitrc
+     pacman -S mate lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+    ;;
+    6)
+    echo "exec i3"  > ~/.xinitrc
+    pacman -S i3 rxvt-unicode dmenu
+    ;;
+    7)
+    echo "exec awesome"  > ~/.xinitrc
+     pacman -S awesome
+    ;;
+    8)
+    echo "exec startdde"  > ~/.xinitrc
+     pacman -S deepin
+    ;;
+    9)
+    echo "exec startlxde"  > ~/.xinitrc
+     pacman -S lxdm-gtk3 lxdm
+    ;;
+esac
