0a1c92934627ec222031838c2d7e3a26b3b1f41e
Ivan <ivdjuran@gmail.com>
2022-03-30 14:33:39 +0200

    chmod +x za nastavak...

--- arch.sh
+++ arch.sh
@@ -4,7 +4,7 @@ mkfs.ext4 /dev/sda3
 mount /dev/sda2 /mnt && mkdir /mnt/home/ && mount /dev/sda3 /mnt/home
 pacstrap -i /mnt base linux linux-firmware sudo nano --noconfirm
 genfstab -U -p /mnt >> /mnt/etc/fstab
+chmod +x nastavak.sh
 cp nastavak.sh /mnt/
 arch-chroot /mnt /bin/bash
-chmod +x nastavak.sh
 ./nastavak.sh
