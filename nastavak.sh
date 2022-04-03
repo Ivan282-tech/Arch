f8ea7a80b148cdef9c195d7c8343860f7021b768
Ivan <ivdjuran@gmail.com>
2022-04-03 20:46:37 +0200

    Dodat unos imena

--- nastavak.sh
+++ nastavak.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+echo "UNESI IME: "
+read ime
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Belgrade /etc/localtime
 hwclock --systohc --utc
-echo ivan > /etc/hostname
-echo "127.0.1.1 localhost.localdomain ivan" >> /etc/hosts
+echo $ime > /etc/hostname
+echo "127.0.1.1 localhost.localdomain $ime" >> /etc/hosts
 pacman -S networkmanager --noconfirm
 systemctl enable NetworkManager 
 echo "SIFRA ZA ROOT KORISNIKA"
@@ -22,15 +24,14 @@ else
     grub-install /dev/sda
     grub-mkconfig -o /boot/grub/grub.cfg
 fi
-
 fallocate -l 3G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo '/swapfile none swap sw 0 0' >> /etc/fstab
-useradd -m -g users -G wheel -s /bin/bash ivan
-echo "SIFRA ZA OBICNOG KORISNIKA!!!"
-passwd ivan
+useradd -m -g users -G wheel -s /bin/bash $ime
+echo "SIFRA ZA $ime!!!"
+passwd $ime
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server --noconfirm
 pacman -S --needed  sddm --noconfirm
